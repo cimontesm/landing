6228d5b757e5aeae76e588ebe8a1113b3fbe474c
Cecilia Montes <136648651+cimontesm@users.noreply.github.com>
2025-10-27 19:47:14 +0000

    guia09 hasta JS: Fetch con async/await

---
+++ js/function.js
@@ -0,0 +1,65 @@
+"use strict";
+
+let fetchProducts = (url) => {
+
+    return fetch(url)
+        .then(response => {
+
+            // Verificar si la respuesta no es exitosa
+            if (!response.ok) {
+                throw new Error(`Error HTTP: ${response.status}`);
+            }
+
+            return response.json();
+
+        })
+        .then(data => {
+
+            // Respuesta exitosa
+            return {
+                success: true,
+                body: data
+            };
+
+        })
+        .catch(error => {
+
+            // Error en la solicitud
+            return {
+                success: false,
+                body: error.message
+            };
+
+        });
+}
+
+let fetchCategories = async (url) => {
+    try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error(`Error HTTP: ${response.status}`);
+        }
+
+        let text = await response.text()
+
+        const parser = new DOMParser();
+        const data = parser.parseFromString(text, "application/xml");
+
+        return {
+            success: true,
+            body: data
+        };
+
+    } catch (error) {
+
+        return {
+            success: false,
+            body: error.message
+        };
+
+    }
+}
+
+export { fetchProducts }
+export { fetchCategories }
